01fd688b46c0bcaf734abe928f7d31f70f1ec599
Shane Daykin <shanedaykin92@gmail.com>
2021-03-02 15:50:01 -0500

    Updates

---
+++ precache-manifest.e42983fbd85c0f2810458fe37079ebb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bb84b89b8b73fc06146",
+    "url": "/new-portfolio/static/css/main.b90e493b.chunk.css"
+  },
+  {
+    "revision": "5bb84b89b8b73fc06146",
+    "url": "/new-portfolio/static/js/main.5bb84b89.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/new-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f45415196359f1c96e5e",
+    "url": "/new-portfolio/static/js/runtime~main.f4541519.js"
+  },
+  {
+    "revision": "4c7a948d4f5cb7b16bff3ff4ea032101",
+    "url": "/new-portfolio/index.html"
+  }
+];
