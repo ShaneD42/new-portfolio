099a0d52661c4d090792f148509907921022ec40
Shane Daykin <shanedaykin92@gmail.com>
2021-03-02 15:09:24 -0500

    commit

---
+++ src/resumeData.js
@@ -0,0 +1,51 @@
+let resumeData = {
+    "imageurl":"https://media-exp1.licdn.com/dms/image/C4E03AQHZbiVhprekgg/profile-displayphoto-shrink_400_400/0/1538414442159?e=1619654400&v=beta&t=Wi3ZAYfOGf2pD3e2uNVN3FcObX-2gy3gQWERhvchgFQ",
+    "name": "Shane Daykin",
+    "role": "Full Stack Web Developer",
+    "roleDescription": " Excited to be at the beginned of my coding journey!", 
+    "social links":[  
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shanedaykin/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ShaneD42",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently learning full stack web development, with plans to begin a career in the field after graduation!",
+    "address":"3520 Edgewood Park Drive",
+
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Cocktail Curator",
+        "imgurl":"images/project1.png",
+        "deployed link:":"https://shaned42.github.io/Project-1/"
+      },
+      {
+        "name":"project2",
+        "description":"Doctor Proctor",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"Note Taker",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"Budget Tracker",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Testimonials coming soon!!!"
+      },
+    ]
+  }
+  
+  export default resumeData
